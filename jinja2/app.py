1a85f3642a3dfbc87c683d4e176f11746f9b673e
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2025-06-16 17:41:09 -0300

    Flask

---
+++ jinja2/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/user/<username>')
+def profile(username):
+    return render_template("profile.html",user=username)
+
+@app.route('/')
+def home():
+    produtos = ["Maçã", "Banana", "Laranja"]
+    logado=True
+    return render_template("home.html", produtos=produtos, logado=logado)
+if __name__ == "__main__":
+    app.run(debug=True)
