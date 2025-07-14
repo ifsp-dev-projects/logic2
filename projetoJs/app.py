44447346afdb76272f369516832b65be7dab8cc6
ana julia <anajcmoser@gmail.com>
2025-07-14 17:40:50 -0300

    adicionando sessoes

--- projetoJs/app.py
+++ projetoJs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, make_response
+from flask import Flask, render_template, request, flash, redirect, url_for, make_response, session
 
 app = Flask(__name__)
 app.secret_key = 'edudela'
@@ -14,25 +14,25 @@ def registrarUsuario():
         email = request.form['email']
         password = request.form['password']
 
+
+        
+
         # Validação simples do lado do servidor
         if not username or not email or not password:
             flash('Todos os campos são obrigatórios', 'danger')
         else:
             flash(f'Usuário {username} cadastrado com sucesso!', 'success')
-            return redirect(url_for('registrarUsuario'))
+            if "usuarios" in session:
+                session["usuarios"].append([username, email, password])
+
+            else:
+                session["usuarios"] = [[username, email, password]]
+            cadastro = session['usuarios']
+            return render_template('formulario.html', cadastro=cadastro)
 
         # flash(): Armazena uma mensagem que será exibida na próxima requisição. Isso
         # é útil para fornecer mensagens ao usuário.
     return render_template('formulario.html')
-
-def armazenarUsuario():
-     #Armazenamento: Os dados dos usuários cadastrados (username, password e senha) deverão ser armazenados em uma lista. Utilize session ou cookies para gerenciar a gravação.
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        resposta = make_response(redirect(url_for('formulario')))
-        resposta.set_cookie('username', username, max_age=60*10)
                            
 if __name__ == '__main__':
     app.run(debug=True)
